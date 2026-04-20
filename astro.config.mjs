e71192d85c1d742c6c2a1d0c544642fb2245a33a
Essential Randomness <essentialrandomn3ss@gmail.com>
2026-04-19 22:52:35 -0700

    listen to all interfaces

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atmosphere.community',
-  base: '/',
-  output: 'static',
-
+  site: "https://atmosphere.community",
+  base: "/",
+  output: "static",
+  server: {
+    host: true,
+  },
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
 });
