254c4c0defb5b25506fed5cacfc6bd9731568a5e
Essential Randomness <essentialrandomn3ss@gmail.com>
2026-04-19 22:39:02 -0700

    add nodejs version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://atmosphere.community',
   base: '/',
   output: 'static',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
